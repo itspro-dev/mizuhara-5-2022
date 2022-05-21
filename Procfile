8c5c67be956ef22e20c3d89fe9bd959e6dbc5037
itspro-dev <84543009+itspro-dev@users.noreply.github.com>
2022-05-22 03:47:18 +0600

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m chizuru_mizuharaBot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE chizuru_mizuharaBot
+:: Enables virtual env mode and then starts Rika
+env\scripts\activate.bat && py -m chizuru_mizuharaBot
